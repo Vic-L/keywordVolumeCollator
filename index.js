1c406a4e0d30f2d61f845ed906414e7de08f6f12
Vic-L <vljc17@gmail.com>
2019-06-16 17:59:36 -0500

    feat: use es6 syntax

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-#!/usr/bin/env node
 const fs = require('fs');
 
-const filePaths = process.argv.splice(2);
+module.exports = () => {
+  const filePaths = process.argv.splice(2);
 
-// check against empty argument
-if (filePaths.length === 0) {
-  console.log("No arguments given: List down the paths of your csv files in the following format:\nkvc /path/to.file1 /path/to.file2");
-  process.exit();
-}
-
-// check that filepath exist
-filePaths.forEach(function (path) {
-  console.log(`Checking if file exist: ${path}`);
-  if(fs.existsSync(path)) {
-    console.log("file exists");
-  } else {
-    console.error(`File does not exist: ${path}`);
+  // check against empty argument
+  if (filePaths.length === 0) {
+    console.log("No arguments given: List down the paths of your csv files in the following format:\nkvc /path/to.file1 /path/to.file2");
     process.exit();
   }
-})
 
-console.log(filePaths);
+  // check that filepath exist
+  filePaths.forEach(function (path) {
+    if(fs.existsSync(path)) {
+      console.log(`File exists: ${path}`);
+    } else {
+      console.error(`File does not exist: ${path}`);
+      process.exit();
+    }
+  })
+
+  console.log(filePaths);
+}
