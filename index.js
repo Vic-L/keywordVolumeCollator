5a1f470c011f5d1953e2df8d03a0291fbbfc7fb4
Vic-L <vljc17@gmail.com>
2019-06-16 17:28:46 -0500

    feat: check against empty argument

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log("test");
+const filePaths = process.argv.splice(2)
+// check against empty argument
+if (filePaths.length === 0) {
+  console.log("No arguments given: List down the paths of your csv files in the following format:\nkvc /path/to.file1 /path/to.file2");
+  process.exit();
+}
+console.log(filePaths);
